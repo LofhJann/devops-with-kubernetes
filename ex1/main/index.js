d18b1c401793f0f049dcc2f7cbb74ae8842b5a15
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2021-02-03 17:44:24 +0200

    Edit for get response

--- ex1/main/index.js
+++ ex1/main/index.js
@@ -1,12 +1,19 @@
 const uuid = require('uuid');
 const randomString = uuid.v4();
+const express = require('express')
+
+const app = express();
+const port = 3000
 
 const printString = () => {
   const ts = new Date().toISOString()
-  console.log(ts + ':', randomString);
-
-  setTimeout(printString, 5000);
+  return ts + ': ' + randomString;
 }
 
-printString()
+app.listen(port, function() {
+  console.log('Server started in port', port)
+})
 
+app.get('/', function (req, res) {
+  res.send(printString())
+})
